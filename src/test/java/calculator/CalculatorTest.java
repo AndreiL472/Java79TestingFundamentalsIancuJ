3a6e861078e3679734c7b60a369975fe788dd4d0
C81 <andreilica094@gmail.com>
2025-04-08 18:24:42 +0300

    as

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,4 +1,30 @@
 package calculator;
 
+import org.exemple.calculator.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    //adunarea ar trebui sa fie corecta pt numere intregi
+    @Test
+    public void testAdunare() {
+        double rezltatActual = calculator.adunare(4,5);
+        System.out.println("S-a intrat in testul pt verificarea adunarii!");
+        //definiti rezultatul actual asteptat -> double rezultatAsteptat = 9;
+        assertEquals(9, rezltatActual); //assert(expected, actual);
+
+    }
+
+    @Test
+    public void testScadere() {
+        double rezltatActual = calculator.scadere(4,5);
+
+        assertEquals(-1, rezltatActual);
+
+
+    }
 }
